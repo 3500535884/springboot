6c49211f98b4279765314eceb96ad2c23106a8d3
3500535884 <35000535884@qq.com>
2019-08-29 21:20:32 +0800

    nothing

---
+++ hello-world/src/main/java/com/raindow/helloworld/Greeting.java
@@ -0,0 +1,24 @@
+package com.raindow.helloworld;
+
+public class Greeting {
+
+    private final long id;
+
+    public long getId() {
+        return id;
+    }
+
+     private final String content;
+
+    public String getContent(){
+        return content;
+    }
+
+    public Greeting( long id,String content){
+        this.id=id;
+        this.content=content;
+    }
+
+
+
+}
